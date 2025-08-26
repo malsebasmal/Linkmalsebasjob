3bef91d84e36f3f10169211581a3c8f16922d381
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-08-26 18:38:26 -0500

    add: the main and nav

--- src/metadata/metadata.ts
+++ src/metadata/metadata.ts
@@ -1,12 +1,12 @@
 const metadata = {
-  title: "Malsebasmal (Antonio Maldonado E.) Job Links",
-  description:
-    "All my job links in one place",
+  title: "Antonio Maldonado E. Job Links",
+  description: "All my job links in one place",
+  keywords: "malsebasmal, Malsebasmal, Antonio Maldonado, Antonio Maldonado E., desarrollador, portfolio, linktree, enlaces, enlaces profesionales",
+  author: "Antonio Maldonado Echevarria",
   canonical: "https://fivebrains.pages.dev",
   og: {
-    title: "Malsebasmal (Antonio Maldonado E.) Job Links",
-    description:
-      "All my links in one place",
+    title: "Antonio Maldonado E. Job Links",
+    description: "All my links in one place",
     url: "https://fivebrains.pages.dev",
     image: 'https://fivebrains.pages.dev/assets/og-image.png',
     type: 'website'
