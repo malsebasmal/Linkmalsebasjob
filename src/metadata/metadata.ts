ab73a469a1050dda5ffa855c2a813d39b24a3225
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-08-26 16:54:27 -0500

    start

---
+++ src/metadata/metadata.ts
@@ -0,0 +1,16 @@
+const metadata = {
+  title: "Malsebasmal (Antonio Maldonado E.) Job Links",
+  description:
+    "All my job links in one place",
+  canonical: "https://fivebrains.pages.dev",
+  og: {
+    title: "Malsebasmal (Antonio Maldonado E.) Job Links",
+    description:
+      "All my links in one place",
+    url: "https://fivebrains.pages.dev",
+    image: 'https://fivebrains.pages.dev/assets/og-image.png',
+    type: 'website'
+  },
+} as const
+
+export default metadata
