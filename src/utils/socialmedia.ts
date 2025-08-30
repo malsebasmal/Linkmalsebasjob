70c4212b6f024447dae1b0f4bc818161934fdb06
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-08-30 16:36:21 -0500

    fix:  the template literals in tailwind

--- src/utils/socialmedia.ts
+++ src/utils/socialmedia.ts
@@ -1,3 +1,4 @@
+import GitHub from "@images/icons/socials/GitHub.astro"
 import LinkedIn from "@images/icons/socials/LinkedIn.astro"
 import BlueSky from "@images/icons/socials/BlueSky.astro"
 import Dailydev from "@images/icons/socials/Dailydev.astro"
@@ -7,42 +8,60 @@ import Hashnode from "@images/icons/socials/Hashnode.astro"
 
 const socialmedia = [
   {
+    name: "GitHub",
+    url: "https://github.com/malsebasmal",
+    icon: GitHub,
+    colors: {
+      text: "#1f2328"
+    }
+  },
+  {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/antoniomaldonadotech",
     icon: LinkedIn,
     colors: {
-      bg: "bg-[#0a66c2]"
+      text: "#0a66c2"
     }
   },
   {
     name: "BlueSky",
     url: "https://bsky.app/profile/malsebasmal.bsky.social",
     icon: BlueSky,
-    colors: ""
+    colors: {
+      text: "#0085ff"
+    }
   },
   {
     name: "DailyDev",
     url: "https://app.daily.dev/malsebasmal/upvoted",
     icon: Dailydev,
-    colors: ""
+    colors: {
+      text: "#ac1de4"
+    }
   },
   {
     name: "Medium",
     url: "https://medium.com/@malsebasmal",
     icon: Medium,
-    colors: ""
+    colors: {
+      text: "black"
+    }
   },
   {
     name: "Dev",
     url: "https://dev.to/malsebasmal",
     icon: Dev,
-    colors: ""
+    colors: {
+      text: "black"
+    }
   },
   {
     name: "Hashnode",
     url: "https://hashnode.com/@malsebasmal",
     icon: Hashnode,
-    colors: ""
+    colors: {
+      text: "#2563eb"
+    }
   },
 ] as const
 
