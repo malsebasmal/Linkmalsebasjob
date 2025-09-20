26d890fe95ae7950f3df76da6800b997d763a1d6
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-09-19 20:20:11 -0500

    optimized: the image background for dark mode | fix: the margin for footer and width for pop up

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -33,12 +33,12 @@ const Share = () =>  {
 
       {isOpen && (
         <div className="text-black font-kode dark:text-white fixed inset-0 bg-black/30 z-50 flex items-center justify-center">
-          <div className="rounded-2xl relative w-xs md:w-md h-fit bg-white dark:bg-black border-2 p-4">
-            <button className="cursor-pointer absolute top-0 right-0 p-4" onClick={handleClose}>
+          <div className="rounded-2xl relative w-3xs sm:w-xs md:w-md h-fit bg-white dark:bg-black border-2 p-4">
+            <button className="cursor-pointer absolute top-0 right-0 p-2" onClick={handleClose}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
             </button>
-              <div className="w-full flex flex-col items-center justify-center gap-4">
-                <p className="text-lg font-bold">✨ Comparte mi tarjeta digital</p>
+              <div className="w-full flex flex-col items-center justify-center mt-4 gap-4">
+                <p className="text-lg font-bold text-center">✨ Comparte mi tarjeta digital</p>
                 <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Grinning%20Face%20with%20Smiling%20Eyes.png" alt="Grinning Face with Smiling Eyes" className="size-50" />
                 <button className="cursor-pointer" onClick={handleCopy}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-link2-icon lucide-link-2"><path d="M9 17H7A5 5 0 0 1 7 7h2"/><path d="M15 7h2a5 5 0 1 1 0 10h-2"/><line x1="8" x2="16" y1="12" y2="12"/></svg>
