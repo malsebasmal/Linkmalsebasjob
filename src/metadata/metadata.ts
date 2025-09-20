f93217b8e18197736d08b58e00339a46f0abaa93
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-09-19 20:47:49 -0500

    fix: metadata

--- src/metadata/metadata.ts
+++ src/metadata/metadata.ts
@@ -3,12 +3,12 @@ const metadata = {
   description: "All my job links in one place",
   keywords: "malsebasmal, Malsebasmal, Antonio Maldonado, Antonio Maldonado E., desarrollador, portfolio, linktree, enlaces, enlaces profesionales",
   author: "Antonio Maldonado Echevarria",
-  canonical: "https://fivebrains.pages.dev",
+  canonical: "https://linkmalsebasjob.vercel.app",
   og: {
     title: "Antonio Maldonado E. Job Links",
     description: "All my links in one place",
-    url: "https://fivebrains.pages.dev",
-    image: 'https://fivebrains.pages.dev/assets/og-image.png',
+    url: "https://linkmalsebasjob.vercel.app",
+    image: "https://linkmalsebasjob.vercel.app/linkmalsebasjob.webp",
     type: 'website'
   },
 } as const
